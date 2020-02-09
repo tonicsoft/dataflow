6d22c0a3373359c95a0355820ac2de945b41d2dc
tonicsoft <tonicsoft@users.noreply.github.com>
2020-02-09 13:45:37 +0000

    improve async test tools

--- src/test/kotlin/org/tonicsoft/dataflow/AsyncFlowTest.kt
+++ src/test/kotlin/org/tonicsoft/dataflow/AsyncFlowTest.kt
@@ -2,32 +2,57 @@ package org.tonicsoft.dataflow
 
 import assertk.assertThat
 import assertk.assertions.hasClass
+import assertk.assertions.hasSize
 import assertk.assertions.isEqualTo
 import assertk.assertions.isTrue
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.util.concurrent.CompletableFuture
+import java.util.*
+
+class TestAsyncComputation<T>(private val emitter: ObservableEmitter<T>) {
+    var cancelled = false
+        private set
+
+    init {
+        emitter.setCancellable { cancelled = true }
+    }
+
+    fun provideResult(result: T) {
+        emitter.onNext(result)
+        emitter.onComplete()
+    }
+
+    fun provideError() {
+        emitter.onError(RuntimeException("foo"))
+    }
+}
+
+fun <T> Queue<TestAsyncComputation<T>>.scheduleAsync() = Observable.create<T> {
+    this.add(TestAsyncComputation(it))
+}
 
 class SourceIntoAsyncFlow {
     val context = Context()
     val source = context.makeNode(1)
     val flow = context.makeNode<Int>()
-    val asyncComputation = CompletableFuture<Int>()
+    val asyncComputations: Queue<TestAsyncComputation<Int>> = ArrayDeque()
 
     @BeforeEach
     fun connectInputs() {
-        flow.connectAsync(source) { Observable.fromCompletionStage(asyncComputation).doOnDispose{ asyncComputation.cancel(true)} }
+        flow.connectAsync(source) { asyncComputations.scheduleAsync() }
     }
 
     @Test
     fun initialStateIsComputing() {
         assertThat(flow.state).hasClass(NodeStreamState.Computing::class)
+        assertThat(asyncComputations.size).isEqualTo(1)
     }
 
     @Test
     fun becomeValidWhenComputationIsCompleted() {
-        asyncComputation.complete(3)
+        asyncComputations.remove().provideResult(3)
         assertThat(flow.state).hasClass(NodeStreamState.Valid::class)
         assertThat(flow.value).isEqualTo(3)
     }
@@ -35,7 +60,9 @@ class SourceIntoAsyncFlow {
     @Test
     fun computationIsCancelledWhenSourceIsChanged() {
         source.value = 2
-        assertThat(asyncComputation.isCancelled()).isTrue()
+        assertThat(flow.state).hasClass(NodeStreamState.Computing::class)
+        assertThat(asyncComputations).hasSize(2)
+        assertThat(asyncComputations.remove().cancelled).isTrue()
     }
 }
 
@@ -44,11 +71,11 @@ class SourceToAsyncToSync {
     val source = context.makeNode(1)
     val asyncFlow = context.makeNode<Int>()
     val syncFlow = context.makeNode<Int>()
-    val asyncComputation = CompletableFuture<Int>()
+    val asyncComputations: Queue<TestAsyncComputation<Int>> = ArrayDeque()
 
     @BeforeEach
     fun connectInputs() {
-        asyncFlow.connectAsync(source) { Observable.fromCompletionStage(asyncComputation).doOnDispose{ asyncComputation.cancel(true)} }
+        asyncFlow.connectAsync(source) { asyncComputations.scheduleAsync() }
         syncFlow.connect(asyncFlow) { it }
     }
 
@@ -59,7 +86,8 @@ class SourceToAsyncToSync {
 
     @Test
     fun whenComputationCompletedLeafIsCompleted() {
-        asyncComputation.complete(3)
+        assertThat(asyncComputations).hasSize(1)
+        asyncComputations.remove().provideResult(3)
         assertThat(syncFlow.value).isEqualTo(3)
         assertThat(syncFlow.state).hasClass(NodeStreamState.Valid::class)
     }
@@ -72,12 +100,12 @@ class AsyncFlowAsBaseOfDiamond {
     val passThroughLeft = context.makeNode<Int>()
     val passThroughRight = context.makeNode<Int>()
     val summer = context.makeNode<Int>()
-    val asyncComputation = CompletableFuture<Int>()
+    val asyncComputations: Queue<TestAsyncComputation<Int>> = ArrayDeque()
     var count = 0
 
     @BeforeEach
     fun connectInputs() {
-        diamondBase.connectAsync(source) { Observable.fromCompletionStage(asyncComputation).doOnDispose{ asyncComputation.cancel(true)} }
+        diamondBase.connectAsync(source) { asyncComputations.scheduleAsync() }
         passThroughLeft.connect(diamondBase) { it }
         passThroughRight.connect(diamondBase) { it }
         summer.connect(passThroughLeft, passThroughRight) { left, right -> count++; left + right }
@@ -92,7 +120,7 @@ class AsyncFlowAsBaseOfDiamond {
     @Test
     fun leafOnlyRecomputedOnceWhenAsyncCompleted() {
         count = 0
-        asyncComputation.complete(1)
+        asyncComputations.remove().provideResult(1)
         assertThat(summer.value).isEqualTo(2)
         assertThat(count).isEqualTo(1)
     }
